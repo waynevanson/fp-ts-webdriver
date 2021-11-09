0500079df8b34b1c6cc2f81ac0dfe8f1c4dcccf6
Wayne Van Son <waynevanson@gmail.com>
2021-11-09 18:31:42 +1100

    chore: add jest-fp-ts

--- jest.config.ts
+++ jest.config.ts
@@ -1,27 +1,44 @@
 import { Config } from "@jest/types"
+import { array as MA } from "fp-ts"
+import { pipe } from "fp-ts/lib/function"
 import { defaults } from "ts-jest/presets"
+
+const SPECS = "**/*.spec.ts"
+const TESTS = "**/*.test.ts"
+
+export const ALL: Config.InitialProjectOptions = {
+  ...defaults,
+  setupFilesAfterEnv: ["@relmify/jest-fp-ts"],
+}
 
 const config: Config.InitialOptions = {
   preset: "ts-jest",
   testEnvironment: "node",
   collectCoverageFrom: ["src/**/*.ts"],
-  globals: { "ts-jest": {} },
-  projects: [
-    {
-      ...defaults,
-      displayName: "chromedriver",
-      globalSetup: "./jest/chromedriver/globalSetup.ts",
-      globalTeardown: "./jest/chromedriver/globalTeardown.ts",
-      globals: { "ts-jest": {}, port: 9000 },
-    },
-    {
-      ...defaults,
-      displayName: "geckodriver",
-      globalSetup: "./jest/geckodriver/globalSetup.ts",
-      globalTeardown: "./jest/geckodriver/globalTeardown.ts",
-      globals: { "ts-jest": {}, port: 7000 },
-    },
-  ],
+  setupFilesAfterEnv: ["@relmify/jest-fp-ts"],
+  projects: pipe(
+    [
+      {
+        displayName: "unit",
+        testMatch: [TESTS],
+      },
+      {
+        displayName: "chromedriver",
+        globalSetup: "./jest/chromedriver/globalSetup.ts",
+        globalTeardown: "./jest/chromedriver/globalTeardown.ts",
+        globals: { "ts-jest": {}, port: 9000 },
+        testMatch: [SPECS],
+      },
+      {
+        displayName: "geckodriver",
+        globalSetup: "./jest/geckodriver/globalSetup.ts",
+        globalTeardown: "./jest/geckodriver/globalTeardown.ts",
+        globals: { "ts-jest": {}, port: 7000 },
+        testMatch: [SPECS],
+      },
+    ],
+    MA.map((most) => ({ ...most, ...ALL }))
+  ),
 }
 
 export default config
