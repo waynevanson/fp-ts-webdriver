c667f6ff14b689f215e93c6295a25b71cee5edbd
Wayne Van Son <waynevanson@gmail.com>
2021-11-10 09:42:50 +1100

    fix: coerce JSON methods correctly

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,19 +1,23 @@
 import { default as crossfetch } from "cross-fetch"
 import { fetchCustom, jsonParser } from "fp-fetch"
-import { either as E } from "fp-ts"
+import { either as E, json as Json } from "fp-ts"
+import { flow } from "fp-ts/lib/function"
 
-export const fetch = fetchCustom<unknown, E.Json>({
+export const fetch = fetchCustom<unknown, Json.Json>({
   parser: jsonParser,
   errorParser: jsonParser,
   fetch: crossfetch,
 })
 
-/**
- * @summary
- * `JSON.stringify` that returns the error as `Left<TypeError>` instead of being uncast.
- */
-export const stringifyJson = (i: unknown) =>
-  E.stringifyJSON(i, (e) => e as TypeError)
+export const stringify = flow(
+  Json.stringify,
+  E.mapLeft((e) => e as TypeError)
+)
+
+export const parse = flow(
+  Json.parse,
+  E.mapLeft((e) => e as SyntaxError)
+)
 
 export const url = E.tryCatchK(
   (...args: ConstructorParameters<typeof URL>): URL => new URL(...args),
@@ -22,7 +26,7 @@ export const url = E.tryCatchK(
 
 export function tap(label: string) {
   return <A>(a: A): A => {
-    console.log({ [label]: a })
+    console.dir({ [label]: a })
     return a
   }
 }
